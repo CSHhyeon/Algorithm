f62f2bde27da28c15ccb40f1f5dc6e0bed3451d1
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-05-26 16:57:23 +0900

    수 정렬하기 2.java

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -5,6 +5,32 @@ public class Main {
 
 	public static void main(String[] args) throws IOException{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+		int N = Integer.parseInt(bf.readLine());
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(bf.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int sorted : arr) {
+			sb.append(sorted).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+}
+
+//Arrays.sort()보다는 Collections.sort()가 더 빠르다.
+// 출력 시 매번 println을 하는 것보다는 StringBuilder를 사용하는 것이 더 빠르고 for(int sorted : arr)로 하는 것이 빠르다.
+
+//Collections.sort 예시
+/*
+  public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
 		int N = Integer.parseInt(bf.readLine());
@@ -22,4 +48,4 @@ public class Main {
 		
 		System.out.println(sb);
 	}
-}
+ */
