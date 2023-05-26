48cbdbc20915a1103aaf50aac32aa25bcd1d29fa
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-05-26 17:42:49 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1724 ms, Memory: 82516 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(bf.readLine());
+		int[][] arr = new int[N][2];
+		
+		for(int i = 0; i < N; i++) {
+			String input = bf.readLine();
+			
+			arr[i][0] = Integer.parseInt(input.split(" ")[0]);
+			arr[i][1] = Integer.parseInt(input.split(" ")[1]);
+		}
+		
+		Arrays.sort(arr, (o1, o2) -> {
+			if(o1[0] == o2[0]) {
+				return o1[1] - o2[1];
+			}
+			else {
+				return o1[0] - o2[0];
+			}
+		});
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i][0] + " " + arr[i][1]);
+		}
+	}
+}
