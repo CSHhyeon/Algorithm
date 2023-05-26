8fe3963ac4aa6a440c0397d76c7748d5f81a9c9f
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-05-26 16:25:44 +0900

    [Bronze II] Title: 수 정렬하기, Time: 180 ms, Memory: 14960 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(bf.readLine());
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(bf.readLine());
+		}
+		
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < N; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
