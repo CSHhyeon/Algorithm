e10bd8f9c2b7ffeba99ca41b3ec63d7d333fef2c
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-06-02 14:46:18 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int cnt = 0;
+	
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(bf.readLine());
+		
+        System.out.println(1);
+		System.out.println(0);
+	}
+
+}
