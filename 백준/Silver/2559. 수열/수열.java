b742f684a10c85b7a7f8a7e4465001b6dff1f5c7
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-06-02 16:10:19 +0900

    [Silver III] Title: 수열, Time: 260 ms, Memory: 23672 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -4,21 +4,26 @@ import java.io.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException{
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken()); //온도를 측정한 전체 날짜의 수
-        int k = Integer.parseInt(st.nextToken()); //합을 구하기 위한 연속적인 날짜의 수
-        
-        int[] arr = new int[n+1];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) arr[i] = arr[i-1]+Integer.parseInt(st.nextToken());
-        int max = -10000001;
-        for (int i = k; i <= n; i++) {
-            int rangeSum = arr[i]-arr[i-k];
-            if (max < rangeSum) max = rangeSum;
-        }
-        System.out.println(max);
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());	//온도를 측정한 전체 날짜의 수
+		int K = Integer.parseInt(st.nextToken());	//합을 구하기 위한 연속적인 날짜의 수
+		
+		st = new StringTokenizer(bf.readLine(), " ");
+		int[] temp = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			temp[i] = temp[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+		//부분합 구하기
+		int max = Integer.MIN_VALUE;
+		for(int i = K; i <= N; i++) {
+			int rangeSum = temp[i] - temp[i - K]; 
+			if(rangeSum > max) max = rangeSum;
+		}
+		
+		System.out.println(max);
 	}
 
 }
