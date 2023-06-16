f21477e5814ff5b587832bc202b36fc30f4c55c2
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-06-17 00:19:52 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+        
+		System.out.println((double)A / B);
+	}
+}
