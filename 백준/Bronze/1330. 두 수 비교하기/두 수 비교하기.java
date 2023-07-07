135a1971db618559daf45b51fd82718ded4d0349
Choi Soo Hyeon <98200722+CSHhyeon@users.noreply.github.com>
2023-07-07 23:46:51 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        if(A > B) System.out.println(">");
+        else if(A < B) System.out.println("<");
+        else System.out.println("==");
+    }
+}
